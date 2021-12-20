5d07279b3c2f9e9c6ecb93333a35151826620060
Imrani-15 <startcareer.15@gmail.com>
2021-12-20 09:20:37 +0530

    18-12-2021

---
+++ Assignments/String_Programming_4_12/Middle_Level_Programs/_41_strip_a_set.py
@@ -0,0 +1,6 @@
+# strip a set of characters from a string
+def space_char():
+    print(sentence.strip(' '))
+
+sentence = input("Enter ur string:")
+space_char()
---
+++ Assignments/String_Programming_4_12/Middle_Level_Programs/_50_spilt_str.py
@@ -0,0 +1,1 @@
+# split a string on the last occurrence of the delimiter
